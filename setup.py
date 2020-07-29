c670efa4c60a6cbcd525cf57c1054b67846ff303
Prateek Gupta <dpsprateek@gmail.com>
2020-07-29 02:58:36 -0400

    Setup file ready

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distrianalysis',
+      version='0.1',
+      description='Gaussian and Binomial Distribution analysis tools',
+      packages=['distrianalysis'],
+      zip_safe=False)
+
