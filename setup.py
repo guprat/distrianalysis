67a4fd360537561b272f101f5f6cabea2fa35d6f
Prateek Gupta <dpsprateek@gmail.com>
2020-07-29 20:59:55 -0400

    Necessary files

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(name='distrianalysis',
       version='0.1',
       description='Gaussian and Binomial Distribution analysis tools',
       packages=['distrianalysis'],
+      author = 'Prateek Gupta',
+      author_email = 'dpsrateek@gmail.com',
       zip_safe=False)
 
